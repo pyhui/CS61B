8b4d3a0a7bd9968afb52934c52753bdc2e4b9ab2
pyhui <replycornucopia@163.com>
2022-12-23 21:48:26 +0800

    修改

--- lab2/IntList/AddConstantTest.java
+++ lab2/IntList/AddConstantTest.java
@@ -19,6 +19,8 @@ public class AddConstantTest {
         assertEquals("3 -> 4 -> 5 -> 6 -> 7", lst.toString());
     }
 
+
+
     @Test
     public void testAddToLargeList() {
         IntList lst = IntList.of(1, 2, 3, 4, 5, 6, 7, 8, 9);
