fb054127e3a416af703a7ed88f0795802b62b5a5
pyhui <replycornucopia@163.com>
2023-02-03 17:00:05 +0800

    fix equals

--- proj1/deque/LinkedListDeque.java
+++ proj1/deque/LinkedListDeque.java
@@ -181,7 +181,7 @@ public class LinkedListDeque<T> implements Deque<T>, Iterable<T> {
                 return false;
             }
             for (int i = 0; i < size; i++) {
-                if (this.get(i) != p.get(i)) {
+                if (!(this.get(i).equals(p.get(i)))) {
                     return false;
                 }
             }
